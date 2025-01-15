1f405f3a733013700cd7caf4ed50b65f20efb9b4
Uchi11 <uchimuhammadr@gmail.com>
2025-01-15 10:16:10 +0700

    many to many

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,26 +1,29 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Eloquent Relationships : Relasi One to One</title>
+    <title>Eloquent Relationships : Relasi One to One & Many To Many</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
-</head>
+</head>       
 <body>
-<!-- Relasi One-to-One (Satu ke Satu) adalah jenis hubungan dalam basis data 
-     di mana satu entitas (baris) di tabel pertama hanya dapat berhubungan
-     dengan satu entitas (baris) di tabel kedua, dan sebaliknya. -->
+<!-- Relasi One-to-One adalah
+     hubungan di mana satu baris dalam tabel A hanya dapat terhubung dengan satu baris di tabel B, dan sebaliknya. -->
+<!-- Relasi One-to-Many
+     adalah hubungan di mana satu baris dalam tabel A dapat terhubung dengan banyak baris di tabel B, tetapi setiap baris di tabel B hanya terhubung dengan satu baris di tabel A. -->
     <div class="container">
         <div class="card mt-5">
             <div class="card-body">
                 <h3 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h3>
-                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One</h5>
+                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One & Many To Many</h5>
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>Nama User</th>
                             <th>Nomor Telepon</th>
+                            <th>Roles</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -28,6 +31,11 @@
                         <tr>
                             <td>{{ $user->name }}</td>
                             <td>{{ $user->phone->phone }}</td>
+                            <td>
+                                @foreach ($user->roles()->get() as $role)
+                                <button class="btn btn-sm btn-primary me-2">{{ $role->name }}</button>
+                                @endforeach
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
